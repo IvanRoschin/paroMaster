48ae3dee8bc2cfec5474fa2527b687606ea086b6
AlexRudnyk <alexander.rudnyk@ukr.net>
2024-03-29 14:58:19 +0200

    added items list markup

---
+++ app/components/items.ts
@@ -0,0 +1,126 @@
+import { IItem } from "types/item/IItem";
+
+const itemImgPath = "../images/itemImg.webp";
+
+export const items: IItem[] = [
+  {
+    _id: "1",
+    imgUrl: itemImgPath,
+    brand: "Philips",
+    model: "JIAYIN JYZ-4P",
+    vendor: "15504",
+    title:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016",
+    description:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016. Оригінальний клапан від компанії Philips. Параметри: 230В, 9-12ВA.",
+    price: 123,
+    isAvailable: true,
+    isCompatible: true,
+    compatibility: ["Philips", "Bosch", "Kenwood"],
+  },
+  {
+    _id: "2",
+    imgUrl: itemImgPath,
+    brand: "Philips",
+    model: "JIAYIN JYZ-4P",
+    vendor: "15504",
+    title:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016",
+    description:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016. Оригінальний клапан від компанії Philips. Параметри: 230В, 9-12ВA.",
+    price: 123,
+    isAvailable: true,
+    isCompatible: true,
+    compatibility: ["Philips", "Bosch", "Kenwood"],
+  },
+  {
+    _id: "3",
+    imgUrl: itemImgPath,
+    brand: "Philips",
+    model: "JIAYIN JYZ-4P",
+    vendor: "15504",
+    title:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016",
+    description:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016. Оригінальний клапан від компанії Philips. Параметри: 230В, 9-12ВA.",
+    price: 123,
+    isAvailable: true,
+    isCompatible: true,
+    compatibility: ["Philips", "Bosch", "Kenwood"],
+  },
+  {
+    _id: "4",
+    imgUrl: itemImgPath,
+    brand: "Philips",
+    model: "JIAYIN JYZ-4P",
+    vendor: "15504",
+    title:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016",
+    description:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016. Оригінальний клапан від компанії Philips. Параметри: 230В, 9-12ВA.",
+    price: 123,
+    isAvailable: true,
+    isCompatible: true,
+    compatibility: ["Philips", "Bosch", "Kenwood"],
+  },
+  {
+    _id: "5",
+    imgUrl: itemImgPath,
+    brand: "Philips",
+    model: "JIAYIN JYZ-4P",
+    vendor: "15504",
+    title:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016",
+    description:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016. Оригінальний клапан від компанії Philips. Параметри: 230В, 9-12ВA.",
+    price: 123,
+    isAvailable: true,
+    isCompatible: true,
+    compatibility: ["Philips", "Bosch", "Kenwood"],
+  },
+  {
+    _id: "6",
+    imgUrl: itemImgPath,
+    brand: "Philips",
+    model: "JIAYIN JYZ-4P",
+    vendor: "15504",
+    title:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016",
+    description:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016. Оригінальний клапан від компанії Philips. Параметри: 230В, 9-12ВA.",
+    price: 123,
+    isAvailable: true,
+    isCompatible: true,
+    compatibility: ["Philips", "Bosch", "Kenwood"],
+  },
+  {
+    _id: "7",
+    imgUrl: itemImgPath,
+    brand: "Philips",
+    model: "JIAYIN JYZ-4P",
+    vendor: "15504",
+    title:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016",
+    description:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016. Оригінальний клапан від компанії Philips. Параметри: 230В, 9-12ВA.",
+    price: 123,
+    isAvailable: true,
+    isCompatible: true,
+    compatibility: ["Philips", "Bosch", "Kenwood"],
+  },
+  {
+    _id: "8",
+    imgUrl: itemImgPath,
+    brand: "Philips",
+    model: "JIAYIN JYZ-4P",
+    vendor: "15504",
+    title:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016",
+    description:
+      "Клапан електромагнітний для парогенератора Philips JIAYIN JYZ-4P 292202199016. Оригінальний клапан від компанії Philips. Параметри: 230В, 9-12ВA.",
+    price: 123,
+    isAvailable: true,
+    isCompatible: true,
+    compatibility: ["Philips", "Bosch", "Kenwood"],
+  },
+];
