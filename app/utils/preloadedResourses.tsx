b33d6b8ec1e012230ed6e112e17ca5196173175b
Ivan Roschin <ivan.roschin86@gmail.com>
2024-03-29 19:30:00 +0200

    header markup

---
+++ app/utils/preloadedResourses.tsx
@@ -0,0 +1,9 @@
+'use client'
+import ReactDom from 'react-dom'
+
+export function PreloadedResourses() {
+	ReactDom.preload('/icons/sprite.svg', { as: 'image' })
+	return null
+}
+
+export default PreloadedResourses
