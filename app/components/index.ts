b4e6e9b4c2264daf17d40698fcdd88316e2333ce
Ivan Roschin <ivan.roschin86@gmail.com>
2024-04-07 18:30:10 +0300

    add search

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,21 +1,23 @@
-import Cart from "./Cart";
-import Catalog from "./Category";
-import Header from "./Header";
-import ItemListCard from "./ItemListCard";
-import ItemsList from "./ItemsList";
-import Slider from "./Slider";
-import ImagesBlock from "./ImagesBlock";
-import ImagesUpload from "./ImagesUpload";
-import AdminForm from "./AdminForm";
+import AdminForm from './AdminForm'
+import Cart from './Cart'
+import Catalog from './Category'
+import Header from './Header'
+import ImagesBlock from './ImagesBlock'
+import ImagesUpload from './ImagesUpload'
+import ItemListCard from './ItemListCard'
+import ItemsList from './ItemsList'
+import Loader from './Loader'
+import Slider from './Slider'
 
 export {
-  Cart,
-  Catalog,
-  Header,
-  ItemListCard,
-  ItemsList,
-  Slider,
-  ImagesBlock,
-  ImagesUpload,
-  AdminForm,
-};
+	AdminForm,
+	Cart,
+	Catalog,
+	Header,
+	ImagesBlock,
+	ImagesUpload,
+	ItemListCard,
+	ItemsList,
+	Loader,
+	Slider,
+}
