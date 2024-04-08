74082a914f9867dcee214c9ff5d41caebb1a90e5
AlexRudnyk <alexander.rudnyk@ukr.net>
2024-04-09 00:13:09 +0300

    adds price filter

--- app/actions/getTest.tsx
+++ app/actions/getTest.tsx
@@ -1,100 +1,101 @@
-'use server'
+"use server";
 
-import { IItem } from '@/types/item/IItem'
-import { connectToDB } from '@/utils/dbConnect'
-import Good from 'model/Good'
-import { revalidatePath } from 'next/cache'
+import { IItem } from "@/types/item/IItem";
+import { connectToDB } from "@/utils/dbConnect";
+import Good from "model/Good";
+import { revalidatePath } from "next/cache";
 
 type Search = {
-	search: string
-	sort: string
-}
+  search: string;
+  sort: string;
+  low: string;
+  high: string;
+};
 
 //** Category search: { "category": {$eq:"cateroryName" } } * /
 //** Vendor search: { "vendor": {$eq:"vendorName" } } * /
 
 export async function getAllGoods(props?: Search) {
-	try {
-		connectToDB()
+  console.log("PROPS", props);
+  try {
+    connectToDB();
 
-		let filter: any = {}
-		if (props?.search) {
-			filter = {
-				$and: [
-					{
-						$or: [
-							{
-								title: {
-									$regex: props?.search,
-									$options: 'i',
-								},
-							},
-							{ vendor: props?.search },
-							{
-								brand: {
-									$regex: props?.search,
-									$options: 'i',
-								},
-							},
-							{
-								compatibility: {
-									$regex: props?.search,
-									$options: 'i',
-								},
-							},
-						],
-					},
-				],
-			}
-		}
+    let filter: any = {};
 
-		let sortOption: any = {}
-		if (props?.sort === 'desc') {
-			sortOption = { price: -1 }
-		} else {
-			sortOption = { price: 1 }
-		}
+    // if (props?.low && props.high) {
+    //   filter = { price: { $gte: Number(props.low), $lte: Number(props.high) } };
+    // }
 
-		let goods: IItem[] = []
+    if (props?.search) {
+      filter = {
+        $and: [
+          {
+            $or: [
+              { title: { $regex: props?.search, $options: "i" } },
+              { vendor: props?.search },
+              { brand: { $regex: props?.search, $options: "i" } },
+              { compatibility: { $regex: props?.search, $options: "i" } },
+            ],
+          },
+          { price: { $gte: Number(props.low), $lte: Number(props.high) } },
+        ],
+      };
+    } else if (props?.low && props.high) {
+      const low = Number(props?.low);
+      const high = Number(props?.high);
 
-		goods = await Good.find(filter).sort(sortOption)
+      filter = { price: { $gte: low, $lte: high } };
+    }
 
-		return goods
-	} catch (error) {
-		console.log(error)
-	}
-	revalidatePath('/')
+    let sortOption: any = {};
+    if (props?.sort === "desc") {
+      sortOption = { price: -1 };
+    } else {
+      sortOption = { price: 1 };
+    }
+
+    let goods: IItem[] = [];
+
+    goods = await Good.find(filter)
+      .sort(sortOption)
+      .find({ $gte: Number(props?.low), $lte: Number(props?.high) });
+
+    return goods;
+  } catch (error) {
+    console.log(error);
+  }
+  revalidatePath("/");
 }
 
 export async function getGoodById(id: string) {
-	try {
-		await connectToDB()
+  try {
+    await connectToDB();
 
-		const good = await Good.findById({ _id: id })
-		return good
-	} catch (error) {
-		console.log(error)
-	}
+    const good = await Good.findById({ _id: id });
+    return good;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function addGood(values: IItem) {
-	try {
-		await connectToDB()
+  try {
+    await connectToDB();
 
-		await Good.create({
-			title: values.title,
-			brand: values.brand,
-			model: values.model,
-			price: values.price,
-			description: values.description,
-			imgUrl: values.imgUrl,
-			vendor: values.vendor,
-			isAvailable: values.isAvailable,
-			isCompatible: values.isCompatible,
-			compatibility: values.compatibility,
-		})
-	} catch (error) {
-		console.log(error)
-	}
-	revalidatePath('/')
+    await Good.create({
+      title: values.title,
+      brand: values.brand,
+      model: values.model,
+      price: values.price,
+      description: values.description,
+      imgUrl: values.imgUrl,
+      vendor: values.vendor,
+      isAvailable: values.isAvailable,
+      isCompatible: values.isCompatible,
+      compatibility: values.compatibility,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  revalidatePath("/");
 }
--- app/components/ItemsList.tsx
+++ app/components/ItemsList.tsx
@@ -1,25 +1,24 @@
-import { getAllGoods } from '@/actions/getTest'
-import { ISearchParams } from '@/types/searchParams'
-import { IItem } from 'types/item/IItem'
-import { ItemListCard } from '.'
+import { getAllGoods } from "@/actions/getTest";
+import { ISearchParams } from "@/types/searchParams";
+import { IItem } from "types/item/IItem";
+import { ItemListCard } from ".";
 
 const ItemsList = async (props: ISearchParams) => {
-	const searchParams = props?.searchParams
+  const searchParams = props?.searchParams;
 
-	const goods = await getAllGoods(searchParams)
-	console.log('goods', goods)
+  const goods = await getAllGoods(searchParams);
 
-	if (goods?.length === 0) {
-		return <p>Товар не знайдений</p>
-	}
+  if (goods?.length === 0) {
+    return <p>Товар не знайдений</p>;
+  }
 
-	return (
-		<ul className='grid grid-cols-4 gap-4'>
-			{goods?.map((item: IItem) => (
-				<ItemListCard key={item._id} item={item} />
-			))}
-		</ul>
-	)
-}
+  return (
+    <ul className="grid grid-cols-4 gap-4">
+      {goods?.map((item: IItem) => (
+        <ItemListCard key={item._id} item={item} />
+      ))}
+    </ul>
+  );
+};
 
-export default ItemsList
+export default ItemsList;
