02c07d6f71bca6645eaf7fbb88c829376bb1074d
Ivan Roschin <ivan.roschin86@gmail.com>
2024-04-09 13:28:30 +0300

    resolve conflict

--- app/components/Sort.tsx
+++ app/components/Sort.tsx
@@ -1,38 +1,38 @@
-"use client";
+'use client'
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useState } from 'react'
 
 const Sort = () => {
-  const [sort, setSort] = useState("");
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
+	const [sort, setSort] = useState('')
+	const searchParams = useSearchParams()
+	const pathname = usePathname()
+	const { replace } = useRouter()
 
-  return (
-    <div>
-      Сортувати за ціною:
-      <select
-        value={sort}
-        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-          const params = new URLSearchParams(searchParams);
-          if (e.target.value) {
-            params.set("sort", e.target.value);
-          } else {
-            params.delete("sort");
-          }
-          setSort(e.target.value);
-          replace(`${pathname}?${params.toString()}`, { scroll: false });
-        }}
-      >
-        <option value="" disabled>
-          Вибрати
-        </option>
-        <option value="asc">Найдешевчі</option>
-        <option value="desc">Найдорожчі</option>
-      </select>
-    </div>
-  );
-};
+	return (
+		<div>
+			Сортувати за ціною:
+			<select
+				value={sort}
+				onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+					const params = new URLSearchParams(searchParams)
+					if (e.target.value) {
+						params.set('sort', e.target.value)
+					} else {
+						params.delete('sort')
+					}
+					setSort(e.target.value)
+					replace(`${pathname}?${params.toString()}`, { scroll: false })
+				}}
+			>
+				<option value='' disabled>
+					Вибрати
+				</option>
+				<option value='asc'>Найдешевчі</option>
+				<option value='desc'>Найдорожчі</option>
+			</select>
+		</div>
+	)
+}
 
-export default Sort;
+export default Sort
