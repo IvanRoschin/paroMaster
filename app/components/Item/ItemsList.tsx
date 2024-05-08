e57dc3301f65cf9d4eb5b33815af12fa1b1c1212
Ivan Roschin <ivan.roschin86@gmail.com>
2024-05-08 03:15:00 +0300

    plain object

--- app/components/Item/ItemsList.tsx
+++ app/components/Item/ItemsList.tsx
@@ -1,44 +1,32 @@
 'use client'
 
 import { getAllGoods } from '@/actions/getTest'
+import { SItem } from '@/types/item/IItem'
 import { ISearchParams } from '@/types/searchParams'
 import useSWR from 'swr'
 import { ItemListCard, Loader } from '..'
 
 const ItemsList = (searchParams: { searchParams: ISearchParams }) => {
-	const { data: goods, error } = useSWR('goods', () => getAllGoods(searchParams))
+	const { data, error } = useSWR('data', () => getAllGoods(searchParams))
 
 	if (error) {
 		console.error('Error fetching goods:', error)
-	}
-	if (!goods) {
-		return <Loader />
+		return <div>Error fetching goods</div>
 	}
 
-	if (goods?.length === 0) {
+	if (!data) {
+		return <Loader />
+	}
+	const goods: SItem[] = JSON.parse(data)
+
+	if (goods.length === 0) {
 		return <h2 className='text-4xl mb-4'>Товар не знайдений</h2>
 	}
 
 	return (
 		<ul className='grid grid-cols-4 gap-4'>
 			{goods?.map(item => (
-				<ItemListCard
-					key={item._id}
-					item={{
-						_id: item._id,
-						category: item.category,
-						imgUrl: item.imgUrl,
-						brand: item.brand,
-						model: item.model,
-						vendor: item.vendor,
-						title: item.title,
-						description: item.description,
-						price: item.price,
-						isAvailable: item.isAvailable,
-						isCompatible: item.isCompatible,
-						compatibility: item.compatibility,
-					}}
-				/>
+				<ItemListCard key={item._id} item={item} />
 			))}
 		</ul>
 	)
