ad441042c36c97514083da0c0ee80cd6bce1cc44
Ivan Roschin <ivan.roschin86@gmail.com>
2024-05-31 14:30:13 +0300

    plain object

--- app/components/Cart/CartItem.tsx
+++ app/components/Cart/CartItem.tsx
@@ -4,6 +4,7 @@ import { getGoodById } from '@/actions/getTest'
 import { SItem } from '@/types/item/IItem'
 import { useShoppingCart } from 'app/context/ShoppingCartContext'
 import Image from 'next/image'
+import { IoMdClose } from 'react-icons/io'
 import useSWR from 'swr'
 import Button from '../Button'
 import Loader from '../Loader'
@@ -34,31 +35,49 @@ const CartItem: React.FC<CartItemProps> = ({ id, quantity }) => {
 	return (
 		<div>
 			<li className='flex flex-col justify-between border border-gray-300 rounded-md p-4 hover:shadow-[10px_10px_15px_-3px_rgba(0,0,0,0.3)] transition-all'>
-				<div className='flex items-center justify-center'>
-					<div className='w-[50px] h-[50px]'>
+				<div className='flex items-center justify-center gap-10'>
+					<div className='w-[125px] h-[125px]'>
+						{/* Displaying an image using the `Image` component */}
 						<Image
 							src={good.imgUrl[0]}
 							alt='item_photo'
-							width={50}
-							height={50}
+							width={125}
+							height={125}
 							className='self-center mb-[30px]'
 						/>
 					</div>
 					<p>{good.title}</p>
 					<div className='flex items-center flex-col gap-10'>
 						<div className='flex items-center justify-center gap-20'>
+							{/* Quantity controls with buttons */}
 							<div className='flex items-center justify-between gap-2'>
 								<Button label='-' onClick={() => decreaseCartQuantity(good._id)} small outline />
 								<span className='text-xl'>{quantity}</span>
 								<Button label='+' onClick={() => increaseCartQuantity(good._id)} small outline />
-							</div>
+							</div>{' '}
+							x <p>{good.price} грн.</p>
 						</div>
-						<Button label='Видалити' onClick={() => removeFromCart(good._id)} />
 					</div>{' '}
+					{/* Calculated total price for the quantity */}
+					<p>{good.price * quantity} грн.</p>
+					{/* Button to remove item from cart */}
+					<button
+						onClick={() => removeFromCart(good._id)}
+						className='
+            p-1 
+            border-[1px] 
+            rounded-full 
+            border-neutral-600 
+            hover:opacity-70 
+            transition 
+            absolute 
+            right-9
+            hover:border-primaryAccentColor
+          '
+					>
+						<IoMdClose size={18} />
+					</button>
 				</div>
-				<p>{good.price} грн.</p>
-
-				{/* Add more item details here (e.g., price, image, etc.) */}
 			</li>
 		</div>
 	)
