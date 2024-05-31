12f072d879b5b5830ced54d969fb8ae414de3290
Ivan Roschin <ivan.roschin86@gmail.com>
2024-05-31 14:51:02 +0300

    search params

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { ISearchParams } from '@/types/searchParams'
 import { getAllGoods } from './actions/getTest'
 import { ItemsList, Slider } from './components'
 
-export default async function Home(searchParams: ISearchParams) {
+export default async function Home({ searchParams }: { searchParams: ISearchParams }) {
 	const goods = await getAllGoods(searchParams)
 
 	let newArray = ''
