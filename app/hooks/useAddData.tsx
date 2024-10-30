f02fa1a46afd7fb3eb4e2f2c269df3efda19e01b
Ivan Roschin <ivan.roschin86@gmail.com>
2024-10-31 01:06:04 +0200

    fix warehouse value

--- app/hooks/useAddData.tsx
+++ app/hooks/useAddData.tsx
@@ -1,14 +1,14 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 type AddAction = (
-	newData: FormData,
+	newData: any,
 ) => Promise<{ success: boolean; message: string; data?: { [key: string]: FormDataEntryValue } }>
 
 export const useAddData = (action: AddAction, key: string) => {
 	const queryClient = useQueryClient()
 
 	return useMutation({
-		mutationFn: (newDataItem: FormData) => action(newDataItem),
+		mutationFn: (newDataItem: any) => action(newDataItem),
 		onSuccess: () => {
 			// @ts-ignore
 			queryClient.invalidateQueries({ queryKey: [key] })
