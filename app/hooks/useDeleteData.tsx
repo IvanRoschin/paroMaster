d1dd2c484511c1d3afd35642140120a43c2d94b5
Ivan Roschin <ivan.roschin86@gmail.com>
2025-04-18 10:58:35 +0300

    fix testi in goodpage

--- app/hooks/useDeleteData.tsx
+++ app/hooks/useDeleteData.tsx
@@ -9,18 +9,10 @@ export const useDeleteData = (action: DeleteAction, key: string) => {
   const queryClient = useQueryClient()
 
   return useMutation({
-    mutationFn: (id: string) => {
-      console.log(`Deleting item with id: ${id}`)
-      return action(id)
-    },
-    onSuccess: (_, variables) => {
-      const id = variables
+    mutationFn: (id: string) => action(id),
+    onSuccess: () => {
       toast.success("Дані видалено!")
-      console.log(`Refetching queries with key: ${key}, id: ${id}`)
-
-      // Принудительное обновление данных
       queryClient.invalidateQueries({ queryKey: [key] })
-      queryClient.refetchQueries({ queryKey: [key] }) // Дополнительный шаг, чтобы принудительно перезагрузить данные
     },
     onError: () => {
       toast.error("Помилка при видаленні")
