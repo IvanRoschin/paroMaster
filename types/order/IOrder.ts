7c70d780265cef1c1a9dac41690ee5b53723ff05
Ivan Roschin <ivan.roschin86@gmail.com>
2025-04-28 23:14:30 +0300

    refactor cart

--- types/order/IOrder.ts
+++ types/order/IOrder.ts
@@ -1,26 +1,6 @@
-import { CartItem } from "../cart/ICartItem"
 import { IGood } from "../good/IGood"
 
 export interface IOrder {
-  _id?: string
-  number: string
-  customer: {
-    name: string
-    surname?: string
-    phone: string
-    email: string
-    city: string
-    warehouse: string
-    payment: string
-    [key: string]: any
-  }
-  warehouse?: string
-  orderedGoods: CartItem[]
-  totalPrice: number
-  status: string
-}
-
-export interface IAdminOrder {
   _id?: string
   number: string
   customer: {
